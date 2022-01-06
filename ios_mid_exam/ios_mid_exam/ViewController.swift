83c4412196fcc0d076496d235305821ce9410d1e
Reffix <reffix@Vladislavs-MacBook-Pro.local>
2022-01-06 08:52:47 -0800

    starting test

---
+++ ios_mid_exam/ios_mid_exam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_mid_exam
+//
+//  Created by Vladislav Tsvetanov on 6.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
